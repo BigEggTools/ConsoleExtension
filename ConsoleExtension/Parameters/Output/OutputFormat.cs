2643b5bd21cf6aa559e024da8f7e73634ba9d506
BigEgg <cd_bigegg@hotmail.com>
2017-02-06 17:44:51 +0800

    Split the console extension library

---
+++ ConsoleExtension/Parameters/Output/OutputFormat.cs
@@ -0,0 +1,13 @@
+﻿namespace BigEgg.ConsoleExtension.Parameters.Output
+{
+    using BigEgg.ConsoleExtension.Parameters.Output.Text;
+
+    internal static class OutputFormat
+    {
+        private static VersionInfo versionInfo = new VersionInfo();
+        private static Header header = new Header();
+
+        public static VersionInfo VERSION_INFO { get { return versionInfo; } }
+        public static Header HEADER { get { return header; } }
+    }
+}
