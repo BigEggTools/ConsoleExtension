51d4f47ece24ad9711fd028999f9a58271813542
BigEgg <cd_bigegg@hotmail.com>
2017-02-07 16:45:48 +0800

    #5 add the test for command help reqeust error

---
+++ ConsoleExtension.Tests/Parameters/Errors/CommandHelpRequestErrorTest.cs
@@ -0,0 +1,20 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Errors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Errors;
+
+    using BigEgg.Tools.ConsoleExtension.Tests.Parameters.FakeParameters;
+
+    [TestClass]
+    public class CommandHelpRequestErrorTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var error = new CommandHelpRequestError("clone", true, typeof(GitClone));
+            Assert.AreEqual(ErrorType.CommandHelpRequest, error.ErrorType);
+            Assert.IsTrue(error.StopProcessing);
+        }
+    }
+}
