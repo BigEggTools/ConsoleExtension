7e5f9987f713f63a0a89ce541bd8aa68980f3f58
BigEgg <cd_bigegg@hotmail.com>
2017-02-07 16:45:20 +0800

    #4 add the test for version reqeust error

---
+++ ConsoleExtension.Tests/Parameters/Errors/VersionRequestErrorTest.cs
@@ -0,0 +1,18 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Errors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Errors;
+
+    [TestClass]
+    public class VersionRequestErrorTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var error = new VersionRequestError();
+            Assert.AreEqual(ErrorType.VersionRequest, error.ErrorType);
+            Assert.IsTrue(error.StopProcessing);
+        }
+    }
+}
