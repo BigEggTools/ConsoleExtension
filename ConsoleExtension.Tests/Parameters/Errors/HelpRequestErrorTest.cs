be724e19e2b31ffbd784f4b0257b09dacedd73e0
BigEgg <cd_bigegg@hotmail.com>
2017-02-07 16:45:55 +0800

    #6 add the test for help reqeust error

---
+++ ConsoleExtension.Tests/Parameters/Errors/HelpRequestErrorTest.cs
@@ -0,0 +1,18 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Errors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Errors;
+
+    [TestClass]
+    public class HelpRequestErrorTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var error = new HelpRequestError();
+            Assert.AreEqual(ErrorType.HelpRequest, error.ErrorType);
+            Assert.IsTrue(error.StopProcessing);
+        }
+    }
+}
