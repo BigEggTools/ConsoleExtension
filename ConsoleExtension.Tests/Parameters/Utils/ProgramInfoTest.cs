b4674193f70ad816c7fc08f17c24dc1fcce97b6c
BigEgg <cd_bigegg@hotmail.com>
2017-02-07 17:29:25 +0800

    Add test for programe info

---
+++ ConsoleExtension.Tests/Parameters/Utils/ProgramInfoTest.cs
@@ -0,0 +1,19 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Utils
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Utils;
+
+    [TestClass]
+    public class ProgramInfoTest
+    {
+        [TestMethod]
+        public void DataTest()
+        {
+            Assert.IsFalse(string.IsNullOrWhiteSpace(ProgramInfo.Default.Title));
+            Assert.IsFalse(string.IsNullOrWhiteSpace(ProgramInfo.Default.Version));
+            Assert.IsFalse(string.IsNullOrWhiteSpace(ProgramInfo.Default.Product));
+            Assert.IsFalse(string.IsNullOrWhiteSpace(ProgramInfo.Default.Copyright));
+        }
+    }
+}
