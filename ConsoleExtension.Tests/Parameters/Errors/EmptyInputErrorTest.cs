b34e01cd9ccbbcea504dd5e8265314b1d1e1e41a
BigEgg <cd_bigegg@hotmail.com>
2017-02-08 19:40:57 +0800

    #20 Add tests for empty input models

---
+++ ConsoleExtension.Tests/Parameters/Errors/EmptyInputErrorTest.cs
@@ -0,0 +1,18 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Errors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Errors;
+
+    [TestClass]
+    public class EmptyInputErrorTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var error = new EmptyInputError();
+            Assert.AreEqual(ErrorType.EmptyInput, error.ErrorType);
+            Assert.IsTrue(error.StopProcessing);
+        }
+    }
+}
---
+++ ConsoleExtension.Tests/Parameters/Output/Text/EmptyInputTest.cs
@@ -0,0 +1,21 @@
+﻿namespace BigEgg.Tools.ConsoleExtension.Tests.Parameters.Output.Text
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using BigEgg.Tools.ConsoleExtension.Parameters.Output.Text;
+
+    [TestClass]
+    public class EmptyInputTest
+    {
+        [TestMethod]
+        public void FormatTest()
+        {
+            var header = new EmptyInput();
+            var text = header.Format(80);
+
+            var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            Assert.AreEqual(2, lines.Length);
+        }
+    }
+}
